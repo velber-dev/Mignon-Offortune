a73307dae55c437dd8115fcefb33a94e10f952fd
VelBer2005 <velber2005@gmail.com>
2024-02-09 17:39:23 +0100

    fix getting scroll bar by opening hide menu

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
 
-const rootElement = document.getElementById('app-root') as HTMLElement,
+const rootElement = document.getElementById("app-root") as HTMLElement,
     root = ReactDOM.createRoot(rootElement);
 
-root.render(
-    <App />
-);
+root.render(<App />);
